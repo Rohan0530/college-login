930fbc1220e782ca49ac8413fd5be1e2e42cc154
Sskarale <sourabhkarale01@gmail.com>
2025-05-18 17:29:33 +0530

    created student dashboard

--- client/src/components/StudentDashboard.jsx
+++ client/src/components/StudentDashboard.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import '../style/StudentDashboard.css'
+import { useNavigate } from 'react-router-dom'
+
 
 const StudentDashboard = () => {
+
+  let navigate = useNavigate()
+
+  let handleLogout = ()=>{
+    navigate('/')
+  }
   return (
     <div className='student-main_cont'>
       <nav className='navbar-student'>
@@ -10,9 +18,27 @@ const StudentDashboard = () => {
         </div>
         <div className='nav-item-cont'>
             <p>Student Name</p>
-            <button>Logout</button>
+            <button onClick={handleLogout}>Logout</button>
         </div>
       </nav>
+
+      <div className='student-second-cont'>
+        <div className='student-card-cont'>
+          <h1>Student Details</h1><br />
+          <hr />
+          <div className='student-details-cont'>
+          <p>Full Name: Sourabh Karale</p>
+          <p>Email: sourabh@gmail.com</p>
+          <p>Mobile No: 9620017198</p>
+          <p>Course: MCA</p>
+          <p>Resume:</p>
+          </div>
+
+          <button >Edit Profile</button>
+          
+        </div>
+
+      </div>
       
     </div>
   )
